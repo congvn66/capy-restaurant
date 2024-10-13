ed7ee0ff7392d29f75eecb136807833624df1b14
Nguyen Duc Cong <nguyenduccong2005@gmail.com>
2024-10-13 23:01:17 +0700

    Làm giao diện cho admin.
    - Thêm giao diện cho admin của nhà hàng (gồm file php và css style).

---
+++ admin/index.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <title>capy-restaurant home page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- menu section starts-->
+        <div class="menu">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">admin</a></li>
+                    <li><a href="#">foods</a></li>
+                    <li><a href="#">chefs</a></li>
+                    <li><a href="#">orders</a></li>
+                    <li><a href="#">customers</a></li>
+                    <li><a href="#">dining tables</a></li>
+                    <li><a href="#">provides</a></li>
+                    <li><a href="#">suppliers</a></li>
+                    <li><a href="#">waiters</a></li>
+                    <li><a href="#">ingredients</a></li>
+                    <li><a href="#">accounts</a></li>
+                </ul>
+            </div>
+            
+        </div>
+        <!-- menu section ends-->
+
+        <!-- main content section starts-->
+        <div class="main-content">
+            <div class="wrapper">
+                main-content goes here
+            </div>
+        </div>
+        <!-- main content section ends-->
+
+        <!-- footer section starts-->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">capybara. capybared by <a href="#">capybara</a></p>
+            </div>
+        </div>
+        <!-- footer section ends-->
+    </body>
+</html>
